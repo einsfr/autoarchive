67e7119d64909230e704167160c71cba23e16759
Alex <alex@efswdev.ru>
2017-02-20 23:28:40 +0300

    +logging_func

--- utils/cache.py
+++ utils/cache.py
@@ -8,8 +8,9 @@ class CacheMissException(RuntimeWarning):
 
 class HashCache:
 
-    def __init__(self, cache_size: int):
+    def __init__(self, cache_size: int, logging_func: callable = None):
         self._cache = OrderedDict()
+        self._logging_func = logging_func
         self._cache_size = cache_size
         self._cache_hits = 0
         self._cache_misses = 0
@@ -27,13 +28,17 @@ class HashCache:
         try:
             value = self._cache[self._get_hashed_id(item_id)]
         except KeyError:
+            if self._logging_func:
+                self._logging_func('Cache miss')
             self._cache_misses += 1
             raise CacheMissException
         else:
+            if self._logging_func:
+                self._logging_func('Cache hit')
             self._cache_hits += 1
             return value
 
-    def cache_stats(self):
+    def get_stats(self):
         total_requests = self._cache_hits + self._cache_misses
         try:
             ratio = self._cache_hits / total_requests
