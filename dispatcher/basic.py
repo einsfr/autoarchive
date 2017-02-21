0064bae0e7f45b7ef8434da4b2e8449dc8e88875
Alex <alex@efswdev.ru>
2017-02-21 16:42:55 +0300

    directory depth override in action parameters

--- dispatcher/basic.py
+++ dispatcher/basic.py
@@ -33,6 +33,11 @@ class BasicDispatcher:
         self._action_cache = {}  # ACTIONS ARE CACHEABLE - DO NOT FORGET IT - THEY'RE USED MORE THAN ONCE
         self._filter_cache = {}  # FILTERS ARE CACHEABLE - DO NOT FORGET IT - THEY'RE USED MORE THAN ONCE
 
+        self._no_match_files = []
+        self._input_base_dir = ''
+        self._dir_list = []
+        self._file_count = 0
+
     def _fill_patterns_cache(self):
         logging.debug('Filling rules set patterns cache...')
         for reg_exp, *p in self._patterns:
@@ -43,26 +48,118 @@ class BasicDispatcher:
         if self._simulate:
             logging.warning('--- THIS IS A SIMULATION - NO CHANGES WILL BE MADE ---')
         if self._input_is_a_file:
-            raise NotImplementedError  # TODO: FIX
+            self._input_base_dir, filename = os.path.split(self._input_url)
+            self._dir_list = [{'rel_in_dir': '', 'files': [filename]}]
+            self._file_count = 1
         elif self._input_is_a_dir:
-            if self._use_in_dir_as_root:
-                logging.debug('Including input directory to output path...')
-                rel_out_dir = (os.path.split(self._input_url)[1])
-            else:
-                rel_out_dir = ''
-
+            self._input_base_dir = self._input_url
             logging.debug('Building file list...')
-            input_root_len = len(self._input_url)
-            dir_list = []
-            file_count = 0
             for path, dirs, files in os.walk(self._input_url):
                 if len(files):
-                    dir_list.append({'rel_dir': path[input_root_len + 1:], 'files': files})
-                    file_count += len(files)
-            logging.debug('Found {} files(s) in {} directory(ies)'.format(file_count, len(dir_list)))
-
+                    self._dir_list.append({'rel_in_dir': path[len(self._input_url) + 1:], 'files': files})
+                    self._file_count += len(files)
+            logging.debug('Found {} files(s) in {} directory(ies)'.format(self._file_count, len(self._dir_list)))
         else:
             raise ValueError('Basic dispatcher supports only files and directories as input')
+
+        processed_files_count = 0
+        processed_errors = []
+        for d in self._dir_list:
+            for f in d['files']:
+                rel_in_path = os.path.join(d['rel_in_dir'], f)
+                logging.info('Processing file {} of {}: "{}"...'.format(
+                    processed_files_count + 1, self._file_count, rel_in_path))
+                try:
+                    self._dispatch(d['rel_in_dir'], rel_in_path)
+                except PolicyViolationException as e:
+                    raise e
+                except ActionRunException as e:
+                    processed_errors.append((rel_in_path, e))
+                processed_files_count += 1
+        if self._no_match_files and self._policy == 'warning':
+            logging.warning(
+                'Matching patterns were not found for these files ({}):\r\n{}'.format(
+                    len(self._no_match_files),
+                    '\r\n'.join(self._no_match_files)
+                )
+            )
+        errors_count = len(processed_errors)
+        if errors_count:
+            logging.warning('Finished with {} error(s):\r\n{}'.format(
+                errors_count,
+                '\r\n'.join(['{}: {} {}'.format(pe[0], type(pe[1]), str(pe[1])) for pe in processed_errors])
+            ))
+        else:
+            logging.info('Finished without errors')
+
+    def _dispatch(self, rel_in_dir: str, rel_in_path: str):
+        logging.debug('Base input directory: "{}"'.format(self._input_base_dir))
+        logging.info('Searching for matching patterns in rules set for "{}"...'.format(rel_in_path))
+        patterns = self._get_matching_patterns(rel_in_path)
+        if not patterns:
+            logging.info('No matches were found')
+            if self._policy == 'skip':
+                return
+            elif self._policy == 'warning':
+                self._no_match_files.append(rel_in_path)
+            elif self._policy == 'error':
+                raise PolicyViolationException('No matches were found for "{}"'.format(rel_in_path))
+            else:
+                raise ValueError('Unknown policy: {}'.format(self._policy))
+
+        logging.debug('Matches: {}'.format(patterns))
+        abs_in_path = os.path.join(self._input_base_dir, rel_in_path)
+        filtered_patterns = self._filter_patterns(abs_in_path, patterns)
+        for n, p in enumerate(filtered_patterns):
+            action_id = p[2]
+            action_params = p[3]
+            logging.info(
+                'Pattern {} of {}: performing action: {}; action parameters: {}...'.format(
+                    n + 1, len(filtered_patterns), action_id, action_params
+                )
+            )
+
+            logging.debug('Building output directory path...')
+            rel_out_dir_list = []
+            if self._input_is_a_dir and self._use_in_dir_as_root:
+                rel_out_dir_list.append(self._input_base_dir)
+            if 'out_dir' in action_params and action_params['out_dir']:
+                rel_out_dir_list.append(action_params['out_dir'])
+
+            dir_depth = action_params['dir_depth'] if 'dir_depth' in action_params else self._dir_depth
+            logging.debug('Output directory depth is {}'.format(dir_depth))
+            if rel_in_dir and dir_depth > 0:
+                path = rel_in_dir
+                path_list = []
+                while True:
+                    head, tail = os.path.split(path)
+                    path = head
+                    if tail:
+                        path_list.append(tail)
+                    if not head:
+                        break
+                logging.debug('Input path list: {}'.format(path_list))
+                if len(path_list) > 0:
+                    if len(path_list) < dir_depth:
+                        logging.debug('Path list\'s length is less than output directory depth - using it all')
+                        rel_out_dir_list.extend(path_list[::-1])
+                    else:
+                        path_list_slice = path_list[:-dir_depth - 1:-1]
+                        logging.debug('Path list\'s length is greater than or equal to output directory depth - using '
+                                      'slice: {}'.format(path_list_slice))
+                        rel_out_dir_list.extend(path_list_slice)
+            logging.debug('Output relative path list: {}'.format(rel_out_dir_list))
+            out_dir = os.path.abspath(os.path.join(self._conf_out_dir, *rel_out_dir_list))
+            logging.debug('Absolute output directory path: "{}"'.format(out_dir))
+
+            logging.debug('Fetching action object...')
+            action = self._get_action(action_id)
+            logging.debug('Using action object {}'.format(action))
+            action.run(
+                abs_in_path,
+                action_params,
+                out_dir
+            )
 
     def _get_matching_patterns(self, in_path: str) -> list:
         return [p for r, *p in self._patterns_cache if r.match(in_path) is not None]
@@ -85,37 +182,6 @@ class BasicDispatcher:
             self._filter_cache[filter_id] = filter_obj
             return filter_obj
 
-    def _dispatch_file_dir_common(self, in_path: str, out_dir_relative: str):
-        logging.info('Searching for matching patterns in rules set for "{}"...'.format(in_path))
-        patterns = self._get_matching_patterns(in_path)
-        if not patterns:
-            logging.info('No matches were found')
-            if self._policy == 'skip':
-                return
-            elif self._policy == 'warning':
-                self._no_match_files.append(in_path)
-            elif self._policy == 'error':
-                raise PolicyViolationException('No matches were found for "{}"'.format(in_path))
-            else:
-                raise ValueError('Unknown policy: {}'.format(self._policy))
-        logging.debug('Matches: {}'.format(patterns))
-        filtered_patterns = self._filter_patterns(in_path, patterns)
-        for n, p in enumerate(filtered_patterns):
-            action_id = p[2]
-            action_params = p[3]
-            logging.info(
-                'Pattern {} of {}: performing action: {}; action parameters: {}...'.format(
-                    n + 1, len(filtered_patterns), action_id, action_params
-                )
-            )
-            action = self._get_action(action_id)
-            logging.debug('Using action object {}'.format(action))
-            if 'out_dir' in action_params and action_params['out_dir']:
-                out_dir = os.path.abspath(os.path.join(self._conf_out_dir, action_params['out_dir'], out_dir_relative))
-            else:
-                out_dir = os.path.join(self._conf_out_dir, out_dir_relative)
-            action.run(in_path, action_params, out_dir)
-
     def _filter_patterns(self, input_url: str, patterns: list) -> list:
         result = []
         for n, p in enumerate(patterns):
@@ -137,56 +203,3 @@ class BasicDispatcher:
         if len(result) != len(patterns):
             logging.debug('Matches after filtering: {}'.format(result))
         return result
-
-    def _no_matches_warning(self):
-        if self._policy == 'warning' and self._no_match_files:
-            logging.warning(
-                'Matching patterns were not found for these files ({}):\r\n{}'.format(
-                    len(self._no_match_files),
-                    '\r\n'.join(self._no_match_files)
-                )
-            )
-
-    def _dispatch_directory(self):
-
-        processed_files_count = 0
-        processed_errors = []
-        for d in dir_list:
-            if self._dir_depth > 0:
-                path = d['dir']
-                path_list = []
-                while True:
-                    head, tail = os.path.split(path)
-                    path = head
-                    if tail:
-                        path_list.append(tail)
-                    if not head:
-                        break
-                if len(path_list) == 0:
-                    out_dir = out_base_dir
-                else:
-                    if len(path_list) < self._dir_depth:
-                        out_dir = os.path.join(out_base_dir, *path_list[::-1])
-                    else:
-                        out_dir = os.path.join(out_base_dir, *path_list[:-self._dir_depth - 1:-1])
-            else:
-                out_dir = out_base_dir
-            for f in d['files']:
-                input_path = os.path.join(self._input_url, d['dir'], f)
-                processed_files_count += 1
-                logging.info('Processing file {} of {}: "{}"'.format(processed_files_count, file_count, input_path))
-                try:
-                    self._dispatch_file_dir_common(input_path, out_dir)
-                except PolicyViolationException as e:
-                    raise e
-                except ActionRunException as e:
-                    processed_errors.append((input_path, e))
-        self._no_matches_warning()
-        errors_count = len(processed_errors)
-        if errors_count:
-            logging.warning('Finished with {} error(s):\r\n{}'.format(
-                errors_count,
-                '\r\n'.join(['{}: {} {}'.format(pe[0], type(pe[1]), str(pe[1])) for pe in processed_errors])
-            ))
-        else:
-            logging.info('Finished without errors')
